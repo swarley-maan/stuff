0ce42c40063370c0637a3fec359cc5aeebff1123
swarley-maan <xavier98m@gmail.com>
2020-03-11 12:03:52 +0000

    criada interface da atividade mostra mensagem

--- app/src/main/java/pt/ipg/myapplication/MainActivity.java
+++ app/src/main/java/pt/ipg/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipg.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void enviaMensagem(View view) {
+        final Intent intent = new Intent(this, MostraMensagemActivity.class);
         //todo:enviar mensagem
+        
+        startActivity(intent);
     }
 }
