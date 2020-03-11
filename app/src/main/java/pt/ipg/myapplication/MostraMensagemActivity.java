8c2b9533d2dfe5d4f6fd4c460e453eb59d031e2e
swarley-maan <xavier98m@gmail.com>
2020-03-11 11:28:14 +0000

    adicionado actividade para mostrar mensagem

---
+++ app/src/main/java/pt/ipg/myapplication/MostraMensagemActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostraMensagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostra_mensagem);
+    }
+}
